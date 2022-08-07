05dc7e1e5dc13a3bc8fd85b3788447721cb3542c
evelyn maude <emaude@wpi.edu>
2022-08-07 22:14:01 +0000

    unit conversion :(

--- src/nav.cpp
+++ src/nav.cpp
@@ -24,8 +24,8 @@ void Nav::send() {
       QString::asprintf("11,%04d%02d%02d,%02d%02d%02d.%01d,%f,%f,%f,%f,%f,%f\n",
       time.utc_year, time.utc_month, time.utc_day,
       time.utc_hour, time.utc_min, time.utc_ms/1000, time.utc_ms/100%10,
-      pos.latitude, pos.longitude, pos.height,
-      vel.trk_gnd, vel.hor_spd, vel.vert_spd
+      pos.latitude, pos.longitude, pos.height*3.28083989501, // 3.28... to convert metres->feet
+      vel.trk_gnd, vel.hor_spd*1.94384449, vel.vert_spd*196.850393701 //1.94... to convert m/s->knots, 196.85... to convert m/s->fpm
       );
     socket->write(output.toUtf8());
   } else {
